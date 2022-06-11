17677318be9dc521d500b40de0a1bf6a6ea5fc1f
Cem Aydin <cem.aydin@gmx.ch>
2022-06-11 14:13:33 +0200

    added system tray using PySimpleGUI and psgtray

--- speech-typer-gui.py
+++ speech-typer-gui.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
+import argparse
 import time
 import speech_recognition as sr
+import pyaudio
+
+import PySimpleGUI as sg
+from psgtray import SystemTray
 
 from pynput.keyboard import Key, Controller
 
@@ -96,51 +101,111 @@ def on_recognize(text):
     else:
         keyboard.type(textlower + ' ')
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print('~~~')
-        result = recognizer.recognize_google(audio)
-        print('Result: ' + result)
-        on_recognize(result)
-        print('>>>')
-    except sr.UnknownValueError:
-        print('...')
-    except sr.RequestError as e:
-        print('Could not request results from Google Speech Recognition service; {0}'.format(e))
+class SpeechTyper:
 
-def start_typer(device):
-    r = sr.Recognizer()
-    m = None
-    if device:
-        m = sr.Microphone()
-    else:
-        m = sr.Microphone(device)
-    with m as source:
-        #r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
-        r.energy_threshold = 300  # we only need to calibrate once, before we start listening
-        r.dynamic_energy_threshold = False
+    def __init__(self, device):
+        self.device = device
+        self.run_tray()
 
-    # start listening in the background (note that we don't have to do this inside a `with` statement)
-    stop_listening = r.listen_in_background(m, callback)
-    print('listening')
-    # `stop_listening` is now a function that, when called, stops background listening
+    # this is called from the background thread
+    def callback(self, recognizer, audio):
+        # received audio data, now we'll recognize it using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print('~~~')
+            self.tray.change_icon('icons/arrows.png')
+            result = recognizer.recognize_google(audio)
+            print('Result: ' + result)
+            on_recognize(result)
+            print('>>>')
+            self.tray.change_icon('icons/active.png')
+        except sr.UnknownValueError:
+            print('...')
+            self.tray.change_icon('icons/active.png')
+        except sr.RequestError as e:
+            print('Could not request results from Google Speech Recognition service; {0}'.format(e))
+            self.tray.change_icon('icons/active.png')
 
-    # do some unrelated computations for 5 seconds
-    #for _ in range(50): time.sleep(1)  # we're still listening even though the main thread is doing other things
+    def start_typer(self):
+        r = sr.Recognizer()
+        m = None
+        if self.device:
+            m = sr.Microphone()
+        else:
+            m = sr.Microphone(self.device)
+        with m as source:
+            #r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+            r.energy_threshold = 300  # we only need to calibrate once, before we start listening
+            r.dynamic_energy_threshold = False
 
-    # calling this function requests that the background listener stop listening
-    #stop_listening(wait_for_stop=False)
+        # start listening in the background (note that we don't have to do this inside a `with` statement)
+        self.stop_listening = r.listen_in_background(m, self.callback)
+        print('listening')
+        # `stop_listening` is now a function that, when called, stops background listening
 
-    # do some more unrelated things
-    while True: time.sleep(0.1)  # we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
+        # do some unrelated computations for 5 seconds
+        #for _ in range(50): time.sleep(1)  # we're still listening even though the main thread is doing other things
+
+        # calling this function requests that the background listener stop listening
+        #stop_listening(wait_for_stop=False)
+
+        # do some more unrelated things
+        #while True: time.sleep(0.1)  # we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
+
+    def run_tray(self):
+        menu = ['', ['Pause/Resume listening', 'Exit']]
+        tooltip = 'Tooltip'
+
+        layout = [[sg.T('Empty Window', key='-T-')]]
+
+        window = sg.Window('Window Title', layout, finalize=True, enable_close_attempted_event=True, alpha_channel=0)
+        window.hide()
+
+        tray = SystemTray(menu, single_click_events=False, window=window, tooltip=tooltip, icon='icons/active.png', key='-TRAY-')
+        #tray.show_message('System Tray', 'System Tray Icon Started!')
+        self.tray = tray
+        print(sg.get_versions())
+
+        self.start_typer()
+
+        while True:
+            event, values = window.read()
+            # IMPORTANT step. It's not required, but convenient. Set event to value from tray
+            # if it's a tray event, change the event variable to be whatever the tray sent
+            if event == tray.key:
+                event = values[event]  # use the System Tray's event as if was from the window
+
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                break
+
+            #tray.show_message(title=event, message=values)
+
+            if event == 'Pause/Resume listening':
+                if self.stop_listening:
+                    self.stop_listening()
+                    tray.change_icon('icons/inactive.png')
+                    self.stop_listening = None
+                else:
+                    self.start_typer()
+                    tray.change_icon('icons/active.png')
+            # elif event == 'Sad':
+            #     tray.change_icon(sg.EMOJI_BASE64_FRUSTRATED)
+            # elif event == 'Plain':
+            #     tray.change_icon(sg.DEFAULT_BASE64_ICON)
+            # elif event == 'Hide Icon':
+            #     tray.hide_icon()
+            # elif event == 'Show Icon':
+            #     tray.show_icon()
+            #elif event == 'Change Tooltip':
+            #    tray.set_tooltip(values['-IN-'])
+
+        tray.close()  # optional but without a close, the icon may "linger" until moused over
+        window.close()
+
 
 def list_devices():
-    import pyaudio
     p = pyaudio.PyAudio()
     print('Devices:')
     for i in range(p.get_device_count()):
@@ -151,12 +216,12 @@ def main(ARGS):
     if ARGS.list_devices:
         list_devices()
     else:
-        start_typer(ARGS.device)
+        st = SpeechTyper(ARGS.device)
+        #st.start_typer(ARGS.device)
 
 if __name__ == '__main__':
     DEFAULT_SAMPLE_RATE = 16000
 
-    import argparse
     parser = argparse.ArgumentParser(description="Stream from microphone to DeepSpeech using VAD")
 
     parser.add_argument('-l', '--list_devices', action='store_true',
